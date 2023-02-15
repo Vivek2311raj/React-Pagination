49ba4dc2ca302b1ce40a3dc3724065bb40059a21
Vivek <vivek2311raj@gmail.com>
2023-02-15 17:57:08 +0530

    frist commit

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/App.css';
+import Loader from './Loader';
+import PostList from './PostList';
+import PaginationButtonsList from './PaginationButtonsList';
+const App = () => {
+    const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(true);
+
+    const handlePage = (pageNumber) => {
+      setPage(pageNumber);
+    }
+
+  useEffect(() => {
+    const getData = async (page, limit) => {
+      setLoading(true);
+      const data = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`)
+      const res = await data.json()
+      setPosts(res);
+      setLoading(false)
+    }
+    getData(page, 5);
+  }, [page])
+  
+  return (
+    <div id="main">
+      
+      {loading ? <Loader /> : <PostList posts= {posts}/>}
+      <PaginationButtonsList page={page} handlePage={handlePage}/>
+    </div>
+  )
+}
+
+
+export default App;
